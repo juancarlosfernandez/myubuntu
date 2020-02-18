8f4cbcaac3ebb0f880a965b900b3b54ef1ac4b80
juancarlosfernandez <juancarlosfernandez@users.noreply.github.com>
2020-02-18 17:26:45 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -53,7 +53,7 @@ add-apt-repository \
 
 apt update
 
-apt install -y docker-ce
+apt install -y aufs-tools docker-ce
 
 # Instalando docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
